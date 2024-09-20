fceec1dc4ef33f84ae66428721e6931ffcf3692b
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-09-20 21:57:14 +0530

    aaa

---
+++ main.py
@@ -0,0 +1,9 @@
+import accuracy
+from home_screen_user import user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13, user14, user15, user16, user17, user18, user19, user20
+from home_screen_book import book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11, book12, book13, book14, book15, book16, book17, book18, book19, book20, book21, book22, book23, book24, book25, book26, book27, book28, book29, book30, book31, book32, book33, book34, book35, book36, book37, book38, book39, book40, book41, book42, book43, book44, book45, book46, book47, book48, book49, book50
+
+# user1: {book42: 17, book2: 16, book13: 15, book21: 15, book22: 15, book30: 15, book34: 15, book40: 15, book49: 15, book50: 15, book17: 15, book9: 14, book19: 14, book24: 14, book25: 14, book31: 14, book41: 14, book43: 14, book44: 14, book45: 14, book46: 14, book48: 14, book6: 14, book14: 13, book18: 13, book20: 13, book26: 13, book28: 13, book32: 13, book33: 13, book36: 13, book37: 13, book38: 13, book39: 13, book47: 13, book8: 13, book5: 12, book11: 12, book12: 12, book29: 12, book15: 12, book16: 11, book27: 11, book3: 11, book10: 11, book35: 11, book7: 10, book1: 10, book4: 9, book23: 9
+# accuracy.accuracy(user1, book42)
+# accuracy.accuracy(user1, book2)
+# accuracy.accuracy(user1, book25)
+accuracy.accuracy(user1, book34)
